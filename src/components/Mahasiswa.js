7e2935a0048e1d635b95454d255b9de390b6963c
nra26 <najyaanara@gmail.com>
2024-06-06 21:48:17 +0700

    my crud website with reactJS

---
+++ src/components/Mahasiswa.js
@@ -0,0 +1,17 @@
+const Mahasiswa = [
+{
+    id:"1",
+    Name:"Najya Anara Parinsi",
+    Age:"20"
+},{
+    id:"2",
+    Name:"Rahmadani Fitri Tompul",
+    Age:"20"
+},{
+    id:"3",
+    Name:"Siti Asma Tomu",
+    Age:"20"
+}
+]
+
+export default Mahasiswa;
